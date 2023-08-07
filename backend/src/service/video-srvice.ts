224a99d947df5fe8d2552826b2c4ce07f2561980
BigWaffleMonster <rachkov.daniil2001@gmail.com>
2023-08-07 22:12:27 +0300

    start

---
+++ backend/src/service/video-srvice.ts
@@ -0,0 +1,35 @@
+import fs from 'fs'
+import busboy from 'busboy'
+import { Request } from 'express'
+import path from 'path'
+
+class VideoService {
+  async upload(req: Request) {
+    console.log('POST request')
+    const bb = busboy({ headers: req.headers })
+    bb.on('file', (name, file, info) => {
+      const { filename, encoding, mimeType } = info
+      const saveTo = path.join(`busboy-upload-1`)
+      file.pipe(fs.createWriteStream(saveTo))
+      file.on('data', (data) => { }).on('close', () => { })
+    })
+    bb.on('field', (name, val, info) => { })
+    bb.on('close', () => { })
+
+    req.pipe(bb)
+    try {
+    } catch (e) { }
+  }
+
+  async play() {
+    try {
+    } catch (e) { }
+  }
+
+  async getPlayerSettings() {
+    try {
+    } catch (e) { }
+  }
+}
+
+export default new VideoService()
